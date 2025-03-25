2194ef0e38ce4b9e39582645410b8ab1439f8d08
Brinda <brinda1104@gmail.com>
2025-03-25 14:35:21 +0530

    adding dependancies

--- eyeglasses/start.js
+++ eyeglasses/start.js
@@ -3,10 +3,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'eyeglasses')));
 
 app.get('/', async(req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'eyeglasses', 'index.html'));
 });
 
 app.listen(8080, () => {
